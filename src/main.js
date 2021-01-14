40277ac1fbeb203b134f337f765cba2647a89ccb
Александр Шабалин <notabene1991@yandex.ru>
2021-01-14 14:26:53 +0300

    полифилл

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 import "./assets/scss/main.scss";
+import "@babel/polyfill";
 
 Vue.config.productionTip = false;
 
